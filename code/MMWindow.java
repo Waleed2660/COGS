990af61bb9d290e88a0ab15d77acc0c4c8455a88
123 <b.roselle@lancaster.ac.uk>
2020-12-06 22:45:39 +0000

    only small change removed a (

--- code/MMWindow.java
+++ code/MMWindow.java
@@ -8,7 +8,7 @@ public class MMWindow
     public MMWindow(int res1, int res2, String name)
     {
         RenderWindow window = new RenderWindow();
-        window.create(new VideoMode(res1, res2), name);         // creates a new window at 1000x600 with a window name "Main menu"
+        window.create(new VideoMode(res1, res2, name);         // creates a new window at 1000x600 with a window name "Main menu"
         window.setMouseCursorVisible(true); //lets user see cursor
         window.setFramerateLimit(30); //limits the frame rate
     }
