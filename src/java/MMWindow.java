36877abb11a990d03af959772da0a0440725ee63
Waleed2660 <waleed.tariq192@gmail.com>
2021-01-20 11:20:10 +0000

    +Changed window.style to FullScreen +Objects are intractable on all sort of resolutions

--- src/java/MMWindow.java
+++ src/java/MMWindow.java
@@ -13,9 +13,11 @@ public class MMWindow extends RenderWindow
     public MMWindow(int res1, int res2, String name)
     {
         super();
-        this.create(new VideoMode(res1, res2), name);         // creates a new window at 1000x600 with a window name "Main menu"
+        // creates a new window at 1000x600 with a window name "Main menu"
+        // Window.style is set to 8 (Full Screen)
+        this.create(new VideoMode(res1, res2), name, 8);
         this.setMouseCursorVisible(true); //lets user see cursor
         this.setFramerateLimit(30); //limits the frame rate
-    }
 
+    }
 }
