70a9a3958c5e5fdec8ef0631a816ac0a9f4acafb
123 <b.roselle@lancaster.ac.uk>
2021-02-26 14:16:45 +0000

    added foundation of sound framework

---
+++ src/java/Sounds.java
@@ -0,0 +1,10 @@
+/*import org.jsmfl.audio.*;
+
+public class Sounds extends Sounds
+{
+    public void Sounds(String name)
+    {
+        Soundbuffer[] soundB = new Soundbuffer[1];
+        soundB.loadFromFile();
+    }
+}*/
